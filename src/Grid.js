76f4c39a25d8f89d5f589d72e52848ebdbf21622
Zipi-P <97598849+Zipi-P@users.noreply.github.com>
2022-01-12 12:42:18 +0200

    Update Grid.js

--- src/Grid.js
+++ src/Grid.js
@@ -3,20 +3,33 @@ import React from 'react';
 const Grid = ({ config, data }) => (
   <table>
     <thead>
-    <tr>
-      <th>Col 1</th>
-      <th>Col 2</th>
-    </tr>
+      <tr>
+        {
+          config.map((config) => {
+            return (
+              <th>{config.title}</th>
+            )
+          })
+        }
+      </tr>
     </thead>
     <tbody>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
+      {
+        data.map((data) => {
+          return (
+            <tr>
+              {
+                config.map((config) => {
+                  return (
+                    config.component ?
+                      <config.component data={data[config.field]} />
+                      :
+                      <td>{data[config.field]}</td>)
+                })}
+            </tr>
+          )
+        })
+      }
     </tbody>
   </table>
 );
